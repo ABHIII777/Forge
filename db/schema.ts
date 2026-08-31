53689563f68ce1363d6fe84f9f4379abc8494201
ABHIII777 <noob.ssooff@gmail.com>
2026-08-31 10:38:42 +0530

    fix: Column names mis-match fix. Adding required columns and renaming for the same.

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,8 @@ import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: serial("user_id").primaryKey(),
-  name: text("name").notNull(),
+  fullname: text("fullname").notNull(),
+  username: text("username").unique().notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull().unique()
 })
