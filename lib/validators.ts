a65da1a04830a4eca96ad79989c2ca7395558db2
ABHIII777 <noob.ssooff@gmail.com>
2026-09-06 02:39:51 +0530

    Feat: Frontend design for the issues feature

--- lib/validators.ts
+++ lib/validators.ts
@@ -21,14 +21,37 @@ export const createProjectSchema = z.object({
     key: z.string().trim().toUpperCase().regex(/^[A-Z0-9]{2,6}$/),
     workspaceId: z.string().uuid(),
     ownerId: z.string().uuid().optional(),
-    status: z.enum(["planning","active","on_hold","completed","archived"]).default("planning"),
+    status: z.enum(["planning", "active", "on_hold", "completed", "archived"]).default("planning"),
 })
 
 export const createWorkspaceSchema = z.object({
     name: z.string().trim().min(3).max(20),
-    slug : z.string().trim().toLowerCase().regex(/^[a-z0-9-]{2,50}$/).optional(),
+    slug: z.string().trim().toLowerCase().regex(/^[a-z0-9-]{2,50}$/).optional(),
     description: z.preprocess(
         (v) => (v === "" ? undefined : v),
         z.string().trim().min(10).max(2000).optional(),
     ),
+})
+
+export const createIssueSchema = z.object({
+    projectId: z.string().uuid(),
+    title: z.string().trim().min(3).max(200),
+    description: z.preprocess(
+        (v) => (v === "" ? undefined : v),
+        z.string().trim().min(10).max(2000).optional()
+    ),
+
+    assigneeId: z.preprocess(
+        (value) => {
+            if (value === "") return undefined;
+            return value;
+        },
+        z.string().uuid().optional()
+    ),
+
+    priority: z
+        .enum(["low", "medium", "high", "critical"])
+        .default("medium"),
+
+    labelIds: z.array(z.string().uuid()).default([]),
 })
